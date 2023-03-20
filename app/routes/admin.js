42060ac032e87b4133f424d5ed7df873c3471866
kongk1234 <kasper.lindebarrith@gmail.com>
2023-03-20 15:58:03 +0100

    fixed admin refrsh bug

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,8 +1,21 @@
 import Route from '@ember/routing/route';
 
 export default class AdminRoute extends Route {
+  queryParams = {
+    page: {
+      refreshModel: true,
+    },
+  };
   async model(params) {
     if(params.page == 1){
+      let response = await fetch(
+        `https://svend.theredWiking.com/product/${params.page}`
+      );
+      let parsed = await response.json();
+      console.log(parsed);
+  
+      return parsed;
+    }else if(params.page == 2){
       let response = await fetch(
         `https://svend.theredWiking.com/product/${params.page}`
       );
@@ -13,7 +26,7 @@ export default class AdminRoute extends Route {
     }
     else {
       let response = await fetch(
-        `https://svend.theredWiking.com/product/${params.page}`
+        `https://svend.theredWiking.com/product/1`
       );
       let parsed = await response.json();
       console.log(parsed);
